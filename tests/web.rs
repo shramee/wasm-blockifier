d27be812a1b15915f559e41d18a1426c75180ad0
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-09-11 12:43:20 +0800

    use contracts dir

--- tests/web.rs
+++ tests/web.rs
@@ -11,7 +11,7 @@ use wasm_bindgen_test::*;
 #[wasm_bindgen_test]
 fn deploy_world_wasm() {
     debug();
-    let json = include_bytes!("../test-fixtures/world.json");
+    let json = include_bytes!("../contracts/dojo-world-test.json");
     let json = String::from_utf8_lossy(json);
 
     let class_declared = register_class_sierra("0xbeef".into(), json.into());
